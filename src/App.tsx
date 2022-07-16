468db23c51676981815e22dbd9c3aefc65dfb152
Guilherme Illescas <guilhermeillescas@Guilhermes-MacBook-Pro-2.local>
2022-07-16 12:40:38 -0300

    feat: added components into page and added icons lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 
 import "./styles/global.css"
+import styles from "./styles/pages/App.module.css"
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-    </>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main></main>
+      </div>
+    </div>
   )
 }
 
